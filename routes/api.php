ab67c447daec3a951838303e6f11a7cf6ff00d57
Ing. Joe Ronald Florez Rada <joerfrada@gmail.com>
2023-02-09 11:24:58 -0500

    Laravel SAML para Portal Logs

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\AplicacionController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get("/hello", function () {
+    return "Hello World";
+});
+
+Route::group(['middleware' => 'cors'], function() {
+    Route::post('/login', [LoginController::class, 'login'])->name('login');
+
+    Route::get('/aplicacion/getAplicacionesFull', [AplicacionController::class, 'getAplicacionesFull']);
+    Route::post('/aplicacion/getAplicaciones', [AplicacionController::class, 'getAplicaciones']);
+    Route::post('/aplicacion/crearAplicaciones', [AplicacionController::class, 'crearAplicaciones']);
+    Route::post('/aplicacion/actualizarAplicaciones', [AplicacionController::class, 'actualizarAplicaciones']);
+    Route::get('/aplicacion/getTipoAplicaciones', [AplicacionController::class, 'getTipoAplicaciones']);
+});
